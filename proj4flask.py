396f98ffb6aae7cb6476cf0e52b4e7eae85779d9
nathanguevara11 <nathangeagle@hotmail.com>
2025-01-04 15:10:12 -0500

    JN and webiste changes

--- proj4flask.py
+++ proj4flask.py
@@ -8,6 +8,20 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-
+@app.route('/one')
+def one():
+    return render_template('one.html')
+@app.route('/two')
+def two():
+    return render_template('two.html')
+@app.route('/three')
+def three():
+    return render_template('three.html')
+@app.route('/four')
+def four():
+    return render_template('four.html')
+@app.route('/five')
+def five():
+    return render_template('five.html')
 if __name__ == "__main__":
     app.run(debug = True)
